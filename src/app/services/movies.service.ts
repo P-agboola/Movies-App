ab533b1f8dcfb70d3798635b5a5f65fa9a3b8de3
Peter Agboola <peterolamilekan65@gmail.com>
2023-05-01 14:20:56 +0100

    creating slider

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -8,6 +8,6 @@ export class MoviesService {
   constructor(private http: HttpClient) {}
 
   getMovies() {
-   return this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=789b2ff1e4396fe6c7bb147c0a0bedcf')
+   return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=789b2ff1e4396fe6c7bb147c0a0bedcf')
   }
 }
